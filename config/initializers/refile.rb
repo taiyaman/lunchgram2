2a247b8c6a9a6a81ed198c063946aadeb00fe184
taiyaman <taiyaman0306@gmail.com>
2021-08-09 13:24:28 +0000

    rails photo

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -2,10 +2,10 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   region: "ap-northeast-1",
-  bucket: "Rails-photo-123456",
+  bucket: "rails-photo-12345678",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
